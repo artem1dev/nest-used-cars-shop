b5866256346d9d9c1a42ceab16481b7b60d7af6a
Artem <162103655+artem1dev@users.noreply.github.com>
2024-03-06 17:45:42 +0200

    Added modules files

---
+++ src/users/users.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from "@nestjs/common";
+import { Repository } from "typeorm";
+import { User } from "./user.entity";
+import { InjectRepository } from "@nestjs/typeorm";
+
+@Injectable()
+export class UsersService {
+    constructor(
+        @InjectRepository(User)
+        private readonly userRepository: Repository<User>
+    ) {}
+
+    create(email: string, password: string) {
+        const user = this.userRepository.create({email, password})
+
+        return this.userRepository.save(user)
+    }
+}
